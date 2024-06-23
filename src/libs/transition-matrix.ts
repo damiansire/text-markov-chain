fef75ff009abc3aa9a298f9fda71bfd36e9052ea
Damian Sire <damiansire@gmail.com>
2024-06-23 10:09:29 -0300

    feat: change transition matrix states structure to: StateData {   states: { [to: string]: number };   amount: number; }

--- src/libs/transition-matrix.ts
+++ src/libs/transition-matrix.ts
@@ -1,21 +1,37 @@
+interface StateData {
+  states: { [to: string]: number };
+  amount: number;
+}
+
 class TransitionMatrix {
-  transitionMatrix: Record<string, Record<string, number>>;
+  transitionMatrix: Record<string, StateData>;
+  transitionMatrixProb: Record<string, Record<string, number>>;
   constructor() {
     this.transitionMatrix = {};
+    this.transitionMatrixProb = {};
   }
   addElement(from: string, to: string) {
     if (!this.transitionMatrix[from]) {
       this.transitionMatrix[from] = {
-        [to]: 1,
+        states: {
+          [to]: 1,
+        },
+        amount: 1,
       };
-    } else if (!this.transitionMatrix[from][to]) {
-      this.transitionMatrix[from][to] = 1;
+    } else if (!this.transitionMatrix[from].states[to]) {
+      this.transitionMatrix[from].states[to] = 1;
+      this.transitionMatrix[from].amount++;
     } else {
-      this.transitionMatrix[from][to]++;
+      this.transitionMatrix[from].states[to]++;
+      this.transitionMatrix[from].amount++;
     }
   }
+  updateTransitionProb() {}
   getOcurrences(from: string, to: string) {
-    return this.transitionMatrix[from][to];
+    return this.transitionMatrix[from].states[to];
+  }
+  getAmountInState(fromState: string) {
+    return this.transitionMatrix[fromState].amount;
   }
   showMatrix() {
     console.log(this.transitionMatrix);
--- src/tests/transition-matrix.spec.ts
+++ src/tests/transition-matrix.spec.ts
@@ -1,10 +1,10 @@
-import TransitionMatrix from "../libs/transition-matrix";
+const TransitionMatrixModule = require("../libs/transition-matrix");
 
 describe("TransitionMatrix", () => {
-  let wizard: any;
+  let TransitionMatrix = TransitionMatrixModule.TransitionMatrix;
 
   beforeAll(() => {
-    expect(TransitionMatrix).toBeDefined();
+    expect(TransitionMatrixModule).toBeDefined();
   });
 
   describe("addElement", () => {
@@ -12,7 +12,9 @@ describe("TransitionMatrix", () => {
       const transitionMatrix = new TransitionMatrix();
       transitionMatrix.addElement("passed", "many");
       const ocurrences = transitionMatrix.getOcurrences("passed", "many");
+      const amount = transitionMatrix.getAmountInState("passed");
       expect(ocurrences).toBe(1);
+      expect(amount).toBe(1);
     });
     it("add four elements to the same key", () => {
       const transitionMatrix = new TransitionMatrix();
@@ -21,7 +23,9 @@ describe("TransitionMatrix", () => {
       transitionMatrix.addElement("passed", "many");
       transitionMatrix.addElement("passed", "many");
       const ocurrences = transitionMatrix.getOcurrences("passed", "many");
+      const amount = transitionMatrix.getAmountInState("passed");
       expect(ocurrences).toBe(4);
+      expect(amount).toBe(4);
     });
     it("add three elements with different keys", () => {
       const transitionMatrix = new TransitionMatrix();
@@ -37,6 +41,8 @@ describe("TransitionMatrix", () => {
       expect(ocurrences1).toBe(1);
       const ocurrences2 = transitionMatrix.getOcurrences("more", "than");
       expect(ocurrences2).toBe(1);
+      const amount = transitionMatrix.getAmountInState("passed");
+      expect(amount).toBe(4);
     });
   });
 });
